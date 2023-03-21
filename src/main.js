672068106ce0de256561dfb0c93751eb92e4e8bb
nidalA1992 <nidalakhmedjanov@gmail.com>
2023-03-21 20:08:59 +0500

    fix: add fixes

--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ import favicon from "./assets/favicon.png";
 
 const ar = [kavaca_icon, logo_white, favicon];
 
+const path = location.pathname.slice(1);
+const activeNav = document.querySelectorAll(`[data-nav="${path}"]`);
+activeNav.forEach(item => item.classList.add('active-nav'));
+
 export function initApp () {
   new Select(".select-lang").init();
   new ModalForm().init();
