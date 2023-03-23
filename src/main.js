a658d91c31320201085a8b75180bc6357bf4f77e
nidalA1992 <nidalakhmedjanov@gmail.com>
2023-03-23 20:32:18 +0500

    feat: add page-loader, fix: main page empty path bug

--- src/main.js
+++ src/main.js
@@ -8,9 +8,13 @@ import favicon from "./assets/favicon.png";
 
 const ar = [kavaca_icon, logo_white, favicon];
 
-const path = location.pathname.slice(1);
+const path = location.pathname.match(/\w/gi)?.join('');
 const activeNav = document.querySelectorAll(`[data-nav="${path}"]`);
 activeNav.forEach(item => item.classList.add('active-nav'));
+
+window.addEventListener('load', () => {
+  document.querySelector('.page-loader').remove();
+});
 
 export function initApp () {
   new Select(".select-lang").init();
